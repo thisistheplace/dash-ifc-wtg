f378a4fc2e9352d45f086ccaa056e0843f90a510
Ben Cannell <bencannell@bens-air.home>
2022-08-20 13:32:15 +0100

    Updated version

--- src/DashIfcWtg.jl
+++ src/DashIfcWtg.jl
@@ -3,7 +3,7 @@ module DashIfcWtg
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.2"
+const version = "0.0.3"
 
 include("jl/dashifcwtg.jl")
 
@@ -16,14 +16,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "dash_ifc_wtg.min.js",
-    external_url = "https://unpkg.com/dash_ifc_wtg@0.0.2/dash_ifc_wtg/dash_ifc_wtg.min.js",
+    external_url = "https://unpkg.com/dash_ifc_wtg@0.0.3/dash_ifc_wtg/dash_ifc_wtg.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_ifc_wtg.min.js.map",
-    external_url = "https://unpkg.com/dash_ifc_wtg@0.0.2/dash_ifc_wtg/dash_ifc_wtg.min.js.map",
+    external_url = "https://unpkg.com/dash_ifc_wtg@0.0.3/dash_ifc_wtg/dash_ifc_wtg.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
